51f5f1b3dd0e8c8a7f080ebf294217e2b1262ef0
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-12-17 00:04:59 +0300

    ~check

--- buildSrc/src/main/kotlin/kover-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kover-conventions.gradle.kts
@@ -1,4 +1,5 @@
 import kotlinx.kover.api.*
+import kotlinx.kover.tasks.*
 
 /*
  * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
@@ -11,7 +12,16 @@ subprojects {
     apply(plugin = "kover")
     tasks.withType<Test>().all {
         extensions.configure<KoverTaskExtension> {
-            isEnabled = coverageEnabled
+            isEnabled = true//coverageEnabled
+        }
+    }
+
+    tasks.withType<KoverVerificationTask> {
+        rule {
+//            name = "Minimal line coverage rate in percents"
+            bound {
+                minValue = 85 // COVERED_LINES_PERCENTAGE
+            }
         }
     }
 }
